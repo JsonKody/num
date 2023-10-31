b32ff292e2c0f1a8efd6e8cd15a64786cc0c35b1
JsonKody <daniel8cech@gmail.com>
2023-10-31 18:48:03 +0100

    saved & pushed by npm

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,3 @@ const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia).mount("#app");
-
-// Zamezeni priblizovani na iPadu pri mackani tlacitka
-document.addEventListener("touchend", function (event: TouchEvent): void {
-  if (event.target && (event.target as HTMLElement).tagName === "BUTTON") {
-    if (event.cancelable) {
-      event.preventDefault();
-    }
-  }
-});
