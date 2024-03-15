a4ecb1a6f4a9c697a3d265dea1e969a10dbc6e88
JsonKody <daniel8cech@gmail.com>
2024-03-15 11:13:32 +0100

    added compression plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import { compression } from 'vite-plugin-compression2'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), compression()],
   server: {
     port: 4040
   }
