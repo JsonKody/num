8184203588ca459845a4c5534e2af18a9488c98b
JsonKody <daniel8cech@gmail.com>
2025-03-25 14:11:21 +0100

    Update zavislosti a hlavne Tailwind 4

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import "./style.scss";
+import "./style.css";
 import { pop } from "./directives/Pop";
 import App from "./App.vue";
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,36 @@
 import { defineConfig } from "vite";
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { compression } from "vite-plugin-compression2";
-import { splitVendorChunkPlugin } from "vite";
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), splitVendorChunkPlugin(), compression(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   server: {
     port: 4040,
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            // priradí knihovny do samostatných chunku podle názvu balícku
-            const modules = [
-              "vue",
-              "pinia",
-              "@floating-ui/dom",
-              "lodash-es",
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes("node_modules")) {
+  //           // priradí knihovny do samostatných chunku podle názvu balícku
+  //           const modules = [
+  //             "vue",
+  //             "pinia",
+  //             "@floating-ui/dom",
+  //             "lodash-es",
 
-              // a další knihovny, které by mohly tvorit významné cásti vašeho kódu
-            ];
+  //             // a další knihovny, které by mohly tvorit významné cásti vašeho kódu
+  //           ];
 
-            const chunkName = modules.find((module) => id.includes(module));
-            return chunkName ? `vendor-${chunkName}` : "vendor-others";
-          }
-        },
-        // ...
-      },
-    },
-    chunkSizeWarningLimit: 600, // nebo jakoukoli jinou hodnotu, která je vhodná pro vaši aplikaci
-  },
+  //           const chunkName = modules.find((module) => id.includes(module));
+  //           return chunkName ? `vendor-${chunkName}` : "vendor-others";
+  //         }
+  //       },
+  //       // ...
+  //     },
+  //   },
+  //   chunkSizeWarningLimit: 600, // nebo jakoukoli jinou hodnotu, která je vhodná pro vaši aplikaci
+  // },
 });
