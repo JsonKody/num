b0d4636fb2b61bdcca79cf0ca44ff3e277df429f
jsonkody <daniel8cech@gmail.com>
2025-03-28 13:28:19 +0100

    saved & pushed by pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
